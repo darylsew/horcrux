50942dd076c819f6bfa03c6eb760ac8e3de2dd08
Hunter Goldstein <hdg28@cornell.edu>
2014-09-27 04:10:05 -0400

    various things

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app,passport){
 
   app.get('/', function(req,res){
-    res.render('index', {
+    res.render('horcrux', {
       title: "Home",
     user: req.user
     });
