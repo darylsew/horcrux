12c84e6c40bb7ff97c1ecf98dceb1cd031e1fe15
Hunter Goldstein <hdg28@cornell.edu>
2014-09-27 17:17:08 -0400

    does this do thing?

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(passport.initialize());
 app.use(passport.session()); 
 app.use(flash());
 
-require('./config/passport')(passport);
+require('./config/passport.js')(passport);
 
 require('./app/routes.js')(app,passport);
 
