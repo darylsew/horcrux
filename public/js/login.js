23e0ce38952ead787569dd6dbadbf70612d8c923
Zachary Zimmerman <Oneman2feet@gmail.com>
2014-09-27 15:20:51 -0400

    login script

---
+++ public/js/login.js
@@ -0,0 +1,16 @@
+function submit() {
+  inputs = "blaze";
+  $.ajax ({
+    type: "POST",
+    url: "user_auth",
+    data: inputs,
+    success: function() {
+      alert('logged in');
+    }
+  });
+}
+
+$(document).ready(function(){
+  $("input").keypress(function(e){ if (e.charCode===13) submit(); });
+  $("#submit").click(submit);
+});
