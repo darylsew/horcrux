8316aa48b1bc5a3eeb3334d03910a275f61c677f
Zachary Zimmerman <Oneman2feet@gmail.com>
2014-09-27 21:28:46 -0400

    removed js enter

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-  $("input").keypress(function(e){ if (e.charCode===13) $("input.submit[name=login]").click(); });
+  //$("input").keypress(function(e){ if (e.charCode===13) $("input.submit[name=login]").click(); });
 });
